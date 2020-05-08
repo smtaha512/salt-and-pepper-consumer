3e63f0d00a1f39f668478548db9b9e6ccdec259a
smtaha512 <s.m.taha10@gmail.com>
2020-05-09 00:28:53 +0500

    feat(menu): add dummy menu page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,20 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'menu',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then((m) => m.MenuPageModule),
   },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
