da83e243152b77177af8781747ef24298cf37134
smtaha512 <s.m.taha10@gmail.com>
2020-05-09 01:05:16 +0500

    feat: move tabs page inside pages folder

--- client/projects/consumer/src/app/pages/tabs/tabs-routing.module.ts
+++ client/projects/consumer/src/app/pages/tabs/tabs-routing.module.ts
@@ -6,15 +6,8 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage,
-    children: [
-      { path: '', redirectTo: 'menu' },
-      {
-        path: 'menu',
-        loadChildren: () => import('../menu/menu.module').then((m) => m.MenuPageModule),
-      },
-    ],
-  },
+    component: TabsPage
+  }
 ];
 
 @NgModule({
