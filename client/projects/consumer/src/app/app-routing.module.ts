1938cf8670ef59e78b821e9e400194122fee6061
smtaha512 <s.m.taha10@gmail.com>
2020-05-09 01:36:39 +0500

    feat(app): add route for tabs and make it default

--- client/projects/consumer/src/app/app-routing.module.ts
+++ client/projects/consumer/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/tabs', pathMatch: 'full' },
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then((m) => m.TabsPageModule),
   },
 ];
