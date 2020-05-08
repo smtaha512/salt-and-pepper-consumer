9af960da961187827e0f9e6f4c7c89c19bae00b0
smtaha512 <s.m.taha10@gmail.com>
2020-05-09 01:36:35 +0500

    feat(menu): add dummy menu page in consumer app

--- client/projects/consumer/src/app/pages/tabs/tabs-routing.module.ts
+++ client/projects/consumer/src/app/pages/tabs/tabs-routing.module.ts
@@ -6,8 +6,15 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
-  }
+    component: TabsPage,
+    children: [
+      { path: '', redirectTo: 'menu' },
+      {
+        path: 'menu',
+        loadChildren: () => import('../menu/menu.module').then((m) => m.MenuPageModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
