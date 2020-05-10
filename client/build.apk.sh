cad120c96dbe1ea827ffda40071b370f132976a3
smtaha512 <s.m.taha10@gmail.com>
2020-05-10 17:49:13 +0500

    build: add more files to copy during build

--- client/build.apk.sh
+++ client/build.apk.sh
@@ -13,18 +13,32 @@ do
 
 done
 
-echo ">> ng build --project=$project $ngOpts" 
-#
-ng build --project=$project $ngOpts;
+echo ">> ng build --project=$project $ngOpts" ;
+# ng build --project=$project $ngOpts;
+
 cp ./capacitor.config.json ./capacitor.config.temp.json;
-echo '>> Do not remove temp file. It will be deleted automatically at the end'
+
+echo '>> Do not remove temp file. It will be deleted automatically at the end';
+
 cp ./projects/$project/capacitor.config.json ./capacitor.config.json;
+
 npx cap sync;
+rm ./android/app/src/main/assets/public/stats.json
+rm ./android/app/src/main/assets/public/stats-es2015.json
+
 cp ./capacitor.config.temp.json ./capacitor.config.json;
-rm ./capacitor.config.temp.json;
+rm ./capacitor.config.temp.json
+
+cp ./projects/$project/android/app/build.gradle ./android/app/build.gradle
 cp ./projects/$project/android/app/src/main/res/values/strings.xml ./android/app/src/main/res/values/strings.xml
+cp ./projects/$project/android/app/src/main/AndroidManifest.xml ./android/app/src/main/AndroidManifest.xml
+rm -r ./android/app/src/main/java
+cp -r ./projects/$project/android/app/src/main/java ./android/app/src/main
+
 (cd ./android/ && \
  echo '>> Assembling APK' && ./gradlew assembleDebug && \
  echo '>> Removing old APK from device' && adb uninstall com.saltandpapper.$project.app
  echo '>> Installing APK' && ./gradlew installDebug\
 );
+
+git reset HEAD --hard
