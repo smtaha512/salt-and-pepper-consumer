41dd623eb4ee4ff683ac4c53ca31897df6bb9560
smtaha512 <s.m.taha10@gmail.com>
2020-05-16 01:49:12 +0500

    feat(library): add orders items list in library

--- client/projects/library/src/public-api.ts
+++ client/projects/library/src/public-api.ts
@@ -4,7 +4,6 @@
 
 export * from './lib/components/order-card/order-card.component';
 export * from './lib/components/order-card/order-card.module';
-
 export * from './lib/components/orders-items-list-header/orders-items-list-header.component';
 export * from './lib/components/orders-items-list-header/orders-items-list-header.module';
 export * from './lib/components/orders-items-list-body/orders-items-list-body.component';
