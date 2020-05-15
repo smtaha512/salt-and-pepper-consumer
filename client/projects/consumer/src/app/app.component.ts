08b80a6c059350348ecad75f64b42f637abc4d8e
smtaha512 <s.m.taha10@gmail.com>
2020-05-15 21:01:32 +0500

    feat: animate psuedo splash and ion-router

--- client/projects/consumer/src/app/app.component.ts
+++ client/projects/consumer/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { timer } from 'rxjs';
+import { mapTo, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'consumer';
+
+  constructor(@Inject(DOCUMENT) private readonly document: Document) {
+    timer(4000)
+      .pipe(
+        tap((_) => this.document.querySelector('div').setAttribute('class', 'd-none')),
+        mapTo(true)
+      )
+      .subscribe();
+  }
 }
