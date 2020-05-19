929959995adb0d90bfd60b06411bcb18c4a51bd9
smtaha512 <s.m.taha10@gmail.com>
2020-05-20 02:52:14 +0500

    feat(library): add date range picker compoonent

--- client/projects/library/src/public-api.ts
+++ client/projects/library/src/public-api.ts
@@ -4,9 +4,13 @@
 
 export * from './lib/components/order-card/order-card.component';
 export * from './lib/components/order-card/order-card.module';
+
 export * from './lib/components/orders-items-list-header/orders-items-list-header.component';
 export * from './lib/components/orders-items-list-header/orders-items-list-header.module';
 export * from './lib/components/orders-items-list-body/orders-items-list-body.component';
 export * from './lib/components/orders-items-list-body/orders-items-list-body.module';
 export * from './lib/components/orders-items-list/orders-items-list.component';
 export * from './lib/components/orders-items-list/orders-items-list.module';
+
+export * from './lib/components/date-range-picker/date-range-picker.component';
+export * from './lib/components/date-range-picker/date-range-picker.module';
