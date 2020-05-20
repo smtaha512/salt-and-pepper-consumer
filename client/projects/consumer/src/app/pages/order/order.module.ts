f3cb3f75de6a42cff526fb86133103424a8b0f8d
smtaha512 <s.m.taha10@gmail.com>
2020-05-20 14:42:39 +0500

    feat(consumer): add order details component

--- client/projects/consumer/src/app/pages/order/order.module.ts
+++ client/projects/consumer/src/app/pages/order/order.module.ts
@@ -1,17 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
+import { OrderDetailsModule } from 'library/library';
 import { OrderPageRoutingModule } from './order-routing.module';
-
 import { OrderPage } from './order.page';
-import { OrderInstructionsModule } from '../../components/order-instructions/order-instructions.module';
-import { MyOrdersItemsListModule } from '../../components/my-orders-items-list/my-orders-items-list.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, OrderPageRoutingModule, OrderInstructionsModule, MyOrdersItemsListModule],
+  imports: [CommonModule, FormsModule, IonicModule, OrderPageRoutingModule, OrderDetailsModule],
   declarations: [OrderPage],
 })
 export class OrderPageModule {}
