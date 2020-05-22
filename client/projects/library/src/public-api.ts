5871abfea68a49d8493869f5d046ab1ac84df276
smtaha512 <s.m.taha10@gmail.com>
2020-05-23 01:42:08 +0500

    refactor: change export all to named exports

--- client/projects/library/src/public-api.ts
+++ client/projects/library/src/public-api.ts
@@ -2,21 +2,21 @@
  * Public API Surface of library
  */
 
-export * from './lib/components/order-card/order-card.component';
-export * from './lib/components/order-card/order-card.module';
+export { OrderCardComponent } from './lib/components/order-card/order-card.component';
+export { OrderCardModule } from './lib/components/order-card/order-card.module';
 
-export * from './lib/components/orders-items-list-header/orders-items-list-header.component';
-export * from './lib/components/orders-items-list-header/orders-items-list-header.module';
-export * from './lib/components/orders-items-list-body/orders-items-list-body.component';
-export * from './lib/components/orders-items-list-body/orders-items-list-body.module';
-export * from './lib/components/orders-items-list/orders-items-list.component';
-export * from './lib/components/orders-items-list/orders-items-list.module';
+export { OrdersItemsListHeaderComponent } from './lib/components/orders-items-list-header/orders-items-list-header.component';
+export { OrdersItemsListHeaderModule } from './lib/components/orders-items-list-header/orders-items-list-header.module';
+export { OrdersItemsListBodyComponent } from './lib/components/orders-items-list-body/orders-items-list-body.component';
+export { OrdersItemsListBodyModule } from './lib/components/orders-items-list-body/orders-items-list-body.module';
+export { OrdersItemsListComponent } from './lib/components/orders-items-list/orders-items-list.component';
+export { OrdersItemsListModule } from './lib/components/orders-items-list/orders-items-list.module';
 
-export * from './lib/components/date-range-picker/date-range-picker.component';
-export * from './lib/components/date-range-picker/date-range-picker.module';
+export { DateRangePickerComponent } from './lib/components/date-range-picker/date-range-picker.component';
+export { DateRangePickerModule } from './lib/components/date-range-picker/date-range-picker.module';
 
-export * from './lib/components/order-details/order-details.component';
-export * from './lib/components/order-details/order-details.module';
+export { OrderDetailsComponent } from './lib/components/order-details/order-details.component';
+export { OrderDetailsModule } from './lib/components/order-details/order-details.module';
 
 export { SetupNgRx } from './lib/store/store-factory.module';
 
