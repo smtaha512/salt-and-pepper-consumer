14df72c0b03e5385695a7564bf21261c3e1379b3
smtaha512 <s.m.taha10@gmail.com>
2020-05-23 01:55:59 +0500

    feat(consumer): add basic ngrx entity for menu

--- client/projects/consumer/src/app/reducers/index.ts
+++ client/projects/consumer/src/app/reducers/index.ts
@@ -1,19 +1,14 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+
 import { environment } from '../../environments/environment';
+import { menusFeatureKey, reducer as MenuReducers, State as MenuState } from '../pages/menu/+state/menu.reducer';
 
 export interface State {
-
+  [menusFeatureKey]: MenuState;
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  [menusFeatureKey]: MenuReducers,
 };
-
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
