7a4d771f2bd60dfa5335abb1dc610331aa46a477
smtaha512 <s.m.taha10@gmail.com>
2020-05-22 12:18:40 +0500

    feat: add total and tax in bill

--- client/projects/consumer/src/app/pages/cart/cart.module.ts
+++ client/projects/consumer/src/app/pages/cart/cart.module.ts
@@ -5,20 +5,11 @@ import { IonicModule } from '@ionic/angular';
 
 import { OrdersItemsListModule } from 'dist/library';
 import { CheckoutFormModule } from '../../components/checkout-form/checkout-form.module';
-import { MyOrderDetailsBillModule } from '../../components/my-order-details-bill/my-order-details-bill.module';
 import { CartPageRoutingModule } from './cart-routing.module';
 import { CartPage } from './cart.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CartPageRoutingModule,
-    MyOrderDetailsBillModule,
-    CheckoutFormModule,
-    OrdersItemsListModule,
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, CartPageRoutingModule, CheckoutFormModule, OrdersItemsListModule],
   declarations: [CartPage],
 })
 export class CartPageModule {}
