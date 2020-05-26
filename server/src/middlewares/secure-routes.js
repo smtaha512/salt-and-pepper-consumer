58eb757feaf71db39327d0a94cc8c9068bbc6750
isaadabbasi <itssaadabbasi@gmail.com>
2020-05-26 18:04:21 +0500

    MVP: Add secure routes

--- server/src/middlewares/secure-routes.js
+++ server/src/middlewares/secure-routes.js
@@ -1,22 +1,13 @@
 const JWT = require('jsonwebtoken');
 const fs = require('fs');
-const { unsecureRoutes } = require('../utils/constants');
 
 const PUBLIC_KEY = fs.readFileSync('./certs/jwtRS256.key.pub', 'utf8');
 
 /**
- * @param {{global?: boolean}} config
  * @returns {function(): void}
  */
-function secureRoute(config = {}) {
-  /**
-   * @param {Request} request
-   * @param {Object} response
-   * @param { function():void } next
-   */
+function secureRoute() {
   return function (request, response, next) {
-    if (config.global && unsecureRoutes.some((route) => new RegExp(`^${route}`, 'i').test(request.path))) return next();
-
     const token = request.headers.authorization;
     if (!token) return response.sendStatus(401);
     JWT.verify(token, PUBLIC_KEY, (err) => {
