4611f17e6b1e3a73b61e13ee3d55245b1a9d699d
isaadabbasi <itssaadabbasi@gmail.com>
2020-05-31 22:06:24 +0500

    MVP: enhance user and menus models and their protected routes

--- server/src/middlewares/secure-routes.js
+++ server/src/middlewares/secure-routes.js
@@ -3,28 +3,20 @@ const fs = require('fs');
 
 const { logger, formatLog } = require('../utils/logger');
 const { API_VERBS } = require('../utils/constants');
-const { _ } = require('../utils/libs/index');
+const { idifyRoute } = require('../utils/index');
 
 const PUBLIC_KEY = fs.readFileSync('./certs/jwtRS256.key.pub', 'utf8');
 
 const unprotectedRoutes = [
   {
     method: API_VERBS.GET,
-    params: null,
     qs: null,
-    url: '/api/orders',
+    url: idifyRoute('/api/menus'),
   },
   {
     method: API_VERBS.GET,
-    params: null,
     qs: null,
-    url: '/api/menus',
-  },
-  {
-    method: API_VERBS.GET,
-    params: null,
-    qs: null,
-    url: '/api/items',
+    url: idifyRoute('/api/items'),
   },
 ];
 /**
@@ -35,9 +27,9 @@ function secureRoute() {
     logger.info(formatLog(request.method, request.originalUrl, 'request', 'headers', request.headers));
     if (
       unprotectedRoutes.find((r) => {
-        const sameUrl = r.url === request.originalUrl;
+        const sameUrl = r.url.test(request.originalUrl);
         const sameMethod = r.method === request.method;
-        // * To compare params and qs, uncomment next two lines
+        // * To compare params and qs, _ is lodash, uncomment next two lines
         // const sameParams = _.isEqual(r.params, request.params)
         // const sameQuery = _.isEqual(r.qs, request.query)
         return sameUrl && sameMethod;
