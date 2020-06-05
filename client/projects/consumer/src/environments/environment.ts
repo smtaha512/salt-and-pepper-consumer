61c19a11584e8c4d7d9bf4cdf816cbca0ecdefec
smtaha512 <s.m.taha10@gmail.com>
2020-06-06 00:54:10 +0500

    feat(consumer): add environment interface

--- client/projects/consumer/src/environments/environment.ts
+++ client/projects/consumer/src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import { EnvironmentInterface } from './environment.model';
+
+export const environment: EnvironmentInterface = {
+  production: false,
+  baseUrl: 'http://localhost:4000/api',
 };
 
 /*
