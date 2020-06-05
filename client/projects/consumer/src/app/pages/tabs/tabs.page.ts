29a8764f1fc8fb01cf81a9ccb714270631aa7318
smtaha512 <s.m.taha10@gmail.com>
2020-06-05 21:18:37 +0500

    refactor: remove old dead code

--- client/projects/consumer/src/app/pages/tabs/tabs.page.ts
+++ client/projects/consumer/src/app/pages/tabs/tabs.page.ts
@@ -1,25 +1,17 @@
-import { animate, style, transition, trigger } from '@angular/animations';
 import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { IonTabButton } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [
-    trigger('tabAnimation', [
-      transition(':enter', [style({ opacity: 0 }), animate('150ms 0ms ease', style({ opacity: 1 }))]),
-      transition(':leave', [animate('150ms 0ms ease', style({ opacity: 0 }))]),
-    ]),
-  ],
 })
 export class TabsPage implements OnInit {
   readonly tabs = [
     { tab: 'orders-history', icon: 'calendar-outline', label: 'Recent orders' },
     { tab: 'menu', icon: 'menu-outline', label: 'Menu' },
-    { tab: 'history', icon: 'calendar-outline', label: 'Recent orders' },
     { tab: 'cart', icon: 'cart-outline', label: 'Cart' },
   ];
 
