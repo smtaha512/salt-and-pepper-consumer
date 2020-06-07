24768201263ad38ffb98d6a3f0bf573748f11ea0
smtaha512 <s.m.taha10@gmail.com>
2020-06-07 17:02:52 +0500

    feat: remove customized loader

--- client/projects/library/src/lib/services/loader/loader.service.ts
+++ client/projects/library/src/lib/services/loader/loader.service.ts
@@ -1,33 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AnimationController, LoadingController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
 import { EMPTY, from, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-
-import { loaderAnimation } from './loader.animation';
 
 @Injectable({ providedIn: 'root' })
 export class LoaderService {
   private loaderInstanceCounter = 0;
 
-  constructor(private readonly loadingController: LoadingController, private readonly animationController: AnimationController) {}
+  constructor(private readonly loadingController: LoadingController) {}
 
   presentLoader() {
-    this.loaderInstanceCounter++;
     return from(this.loadingController.getTop()).pipe(
       switchMap((loader) => {
-        if (loader) {
+        if (this.loaderInstanceCounter >= 1 || loader) {
           return of(loader);
         }
+        this.loaderInstanceCounter++;
         return from(
           this.loadingController
-            .create({ backdropDismiss: false, keyboardClose: false, spinner: null, cssClass: ['animated-loader'] })
-            .then((newLoader) =>
-              newLoader.present().then(() => {
-                const animation = loaderAnimation(this.animationController, newLoader.querySelector('animated-loader'));
-                animation.play();
-                return newLoader;
-              })
-            )
+            .create({ backdropDismiss: false, keyboardClose: false, message: 'Please wait!' })
+            .then((newLoader) => newLoader.present().then(() => newLoader))
         );
       })
     );
