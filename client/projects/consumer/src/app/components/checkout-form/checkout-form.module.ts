ba413f00d5c39b01540b59c693a8f876d1c389d3
smtaha512 <s.m.taha10@gmail.com>
2020-06-09 08:47:47 +0500

    feat(consumer): add form group to cart screen

--- client/projects/consumer/src/app/components/checkout-form/checkout-form.module.ts
+++ client/projects/consumer/src/app/components/checkout-form/checkout-form.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { CheckoutFormComponent } from './checkout-form.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CheckoutFormComponent],
   exports: [CheckoutFormComponent],
-  imports: [CommonModule, IonicModule, RouterModule],
+  imports: [CommonModule, IonicModule, RouterModule, ReactiveFormsModule],
 })
 export class CheckoutFormModule {}
