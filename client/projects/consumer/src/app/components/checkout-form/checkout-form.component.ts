e48222ac42e79f88f5d1a8ed7f044dba316a4cf9
smtaha512 <s.m.taha10@gmail.com>
2020-06-09 09:27:14 +0500

    feat(consumer): add first name and last name in checkout form

--- client/projects/consumer/src/app/components/checkout-form/checkout-form.component.ts
+++ client/projects/consumer/src/app/components/checkout-form/checkout-form.component.ts
@@ -1,8 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonInput } from '@ionic/angular';
-import { combineLatest, Subject } from 'rxjs';
-import { startWith, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-checkout-form',
@@ -10,23 +8,21 @@ import { startWith, takeUntil, tap } from 'rxjs/operators';
   styleUrls: ['./checkout-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckoutFormComponent implements OnInit, OnDestroy {
+export class CheckoutFormComponent implements OnInit {
   form: FormGroup;
-  private readonly destroyed$ = new Subject();
 
   constructor(private readonly fb: FormBuilder) {}
 
   ngOnInit() {
     this.buildForm();
-    this.updateFullNameWhenSameAsAbove();
-    this.updateSameAsAboveIfFullNameChanges();
   }
   buildForm() {
     this.form = this.fb.group({
       cardNumber: this.fb.control('', [Validators.required]),
       cvc: this.fb.control('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]),
       email: this.fb.control('', [Validators.required]),
-      fullName: this.fb.control('', [Validators.required]),
+      firstname: this.fb.control('', [Validators.required]),
+      lastname: this.fb.control('', [Validators.required]),
       isNameSameAsAbove: this.fb.control(false),
       mobileNumber: this.fb.control('', [Validators.required]),
       nameOnCard: this.fb.control('', [Validators.required]),
@@ -36,33 +32,6 @@ export class CheckoutFormComponent implements OnInit, OnDestroy {
 
   onSubmit() {
     console.log(this.form.value);
-  }
-
-  updateFullNameWhenSameAsAbove() {
-    this.isNameSameAsAbove.valueChanges
-      .pipe(
-        tap((sameAsAbove) => {
-          if (sameAsAbove) {
-            this.fullName.setValue(this.nameOnCard.value);
-          }
-        }),
-        takeUntil(this.destroyed$)
-      )
-      .subscribe();
-  }
-  updateSameAsAboveIfFullNameChanges() {
-    combineLatest([this.isNameSameAsAbove.valueChanges, this.fullName.valueChanges, this.nameOnCard.valueChanges])
-      .pipe(
-        startWith([this.isNameSameAsAbove.value, this.fullName.value, this.nameOnCard.value]),
-        tap(([isNameSameAsAbove, fullName, nameOnCard]) => {
-          console.log({ isNameSameAsAbove, fullName, nameOnCard });
-          if (fullName !== nameOnCard && isNameSameAsAbove) {
-            this.isNameSameAsAbove.setValue(false);
-          }
-        }),
-        takeUntil(this.destroyed$)
-      )
-      .subscribe();
   }
 
   openMonthYearSelector(ionInput: IonInput) {
@@ -75,20 +44,5 @@ export class CheckoutFormComponent implements OnInit, OnDestroy {
     }
     const [yyyy, mm, yy = yyyy.slice(2, 4)] = value.toString().split('-');
     return `${mm} / ${yy}`;
-  }
-
-  get fullName() {
-    return this.form.get('fullName');
-  }
-
-  get isNameSameAsAbove() {
-    return this.form.get('isNameSameAsAbove');
-  }
-  get nameOnCard() {
-    return this.form.get('nameOnCard');
-  }
-
-  ngOnDestroy(): void {
-    this.destroyed$.next();
   }
 }
