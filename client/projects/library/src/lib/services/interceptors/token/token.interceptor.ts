758e3444bc8a8b96fb3ebdffbd9e8c5caeab801c
smtaha512 <s.m.taha10@gmail.com>
2021-04-24 19:00:43 +0500

    feat: add exempted endpoints in token interceptor

--- client/projects/library/src/lib/services/interceptors/token/token.interceptor.ts
+++ client/projects/library/src/lib/services/interceptors/token/token.interceptor.ts
@@ -10,11 +10,19 @@ export class TokenInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const { url } = req;
-    if (url.includes('/api')) {
+    if (url.includes('/api') && !this.isExempted(req)) {
       return from(this.storage.getToken()).pipe(
         switchMap((token) => next.handle(req.clone({ headers: new HttpHeaders({ authorization: token }) })))
       );
     }
     return next.handle(req);
+  }
+
+  isExempted(req: HttpRequest<any>): boolean {
+    const exemptedAPIs = [
+      { method: 'GET', endpoint: '/api/menus' },
+      { method: 'GET', endpoint: '/api/items' },
+    ];
+    return exemptedAPIs.some((api) => api.method === req.method && req.url.includes(api.endpoint));
   }
 }
