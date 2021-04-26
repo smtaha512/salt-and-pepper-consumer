3c2b502661b69a0af3549b7a1205aa0eee698aa8
smtaha512 <s.m.taha10@gmail.com>
2021-04-26 08:49:19 +0500

    feat: add api to fetch user by id

--- server/src/routes/users/index.js
+++ server/src/routes/users/index.js
@@ -4,9 +4,18 @@ const repositories = require('../../repositories/index');
 const middlewares = require('../../middlewares/index');
 const dbModels = require('../../models/index');
 
+router.get('/users/:id', middlewares.isReqParamValidID, (request, response) => {
+  const { id } = request.params;
+  repositories.users
+    .getUserById(dbModels)(id)
+    .then((user) => response.status(201).send(user));
+});
+
 router.put('/users/:id/polled', middlewares.isReqParamValidID, (request, response) => {
   const { id } = request.params;
   repositories.users
     .updateLastPolledAt(dbModels)(id)
-    .then(() => response.status(204));
+    .then(() => response.status(204).send());
 });
+
+module.exports = router;
