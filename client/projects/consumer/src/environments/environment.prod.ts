be12cc57c3da21db91f621ab104c5aaa8f35a366
smtaha512 <s.m.taha10@gmail.com>
2021-04-27 00:23:51 +0500

    feat: add stripe publishable key in environment.prod

--- client/projects/consumer/src/environments/environment.prod.ts
+++ client/projects/consumer/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'http://localhost:4000/api',
+  stripePublishableKey: 'pk_test_51I3TBKGdvii2zQm7SYmD26490IhsOqV9ljGCxLRzt0dMxeGXBQK7vZvT0YjR68JRyiKXaIspgUFVKIkrVChVCxWh00bncbaR7E',
 };
