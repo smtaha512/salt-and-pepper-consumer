1f4123bc26bf67dc8532db48883ad37ef7346fc6
smtaha512 <s.m.taha10@gmail.com>
2021-04-27 01:40:29 +0500

    feat: change base url for prod envs

--- client/projects/consumer/src/environments/environment.prod.ts
+++ client/projects/consumer/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:4000',
+  baseUrl: 'http://ec2-18-116-73-17.us-east-2.compute.amazonaws.com:4000',
   stripePublishableKey: 'pk_test_51I3TBKGdvii2zQm7SYmD26490IhsOqV9ljGCxLRzt0dMxeGXBQK7vZvT0YjR68JRyiKXaIspgUFVKIkrVChVCxWh00bncbaR7E',
 };
--- client/projects/manager/src/environments/environment.prod.ts
+++ client/projects/manager/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:4000',
+  baseUrl: 'http://ec2-18-116-73-17.us-east-2.compute.amazonaws.com:4000',
 };
