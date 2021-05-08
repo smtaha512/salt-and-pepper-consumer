7a7be781e0705ecee50729bfd2e84ecfdaa0f2ba
smtaha512 <s.m.taha10@gmail.com>
2021-05-08 23:27:24 +0500

    feat: add new prod base url

--- client/projects/consumer/src/environments/environment.prod.ts
+++ client/projects/consumer/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://ec2-18-116-73-17.us-east-2.compute.amazonaws.com:4000',
+  baseUrl: 'https://api.saltnpeppernc.com',
   stripePublishableKey: 'pk_test_51I3TBKGdvii2zQm7SYmD26490IhsOqV9ljGCxLRzt0dMxeGXBQK7vZvT0YjR68JRyiKXaIspgUFVKIkrVChVCxWh00bncbaR7E',
 };
--- client/projects/manager/src/environments/environment.prod.ts
+++ client/projects/manager/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://ec2-18-116-73-17.us-east-2.compute.amazonaws.com:4000',
+  baseUrl: 'https://api.saltnpeppernc.com',
 };
