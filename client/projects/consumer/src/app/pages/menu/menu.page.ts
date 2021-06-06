b91384696e4da83aae81803f4fd4d47b60579d6b
smtaha512 <s.m.taha10@gmail.com>
2021-06-07 01:36:08 +0500

    feat(menu): update selected segment button when slide changes

--- client/projects/consumer/src/app/pages/menu/menu.page.ts
+++ client/projects/consumer/src/app/pages/menu/menu.page.ts
@@ -19,12 +19,12 @@ import { MenuService } from './services/menu.service';
 export class MenuPage implements OnInit, OnDestroy {
   private readonly subs = new Subscription();
   current$ = new BehaviorSubject('');
-  menus$: Observable<MenuInterface[]> = this.menuService.menuWithItems().pipe(
-    filter(isNotEmpty),
-    tap(([menu]) => this.current$.next(menu?._id)),
-    shareReplay({ refCount: true, bufferSize: 1 })
-  );
+  menus$: Observable<MenuInterface[]> = this.menuService
+    .menuWithItems()
+    .pipe(filter(isNotEmpty), shareReplay({ refCount: true, bufferSize: 1 }));
   menuItems$: Observable<ItemInterface[]> = of([]);
+
+  private initalItems: { element: HTMLIonSegmentButtonElement; distanceFromOrigin: number }[];
 
   constructor(
     @Inject(DOCUMENT) private readonly document: Document,
@@ -40,7 +40,13 @@ export class MenuPage implements OnInit, OnDestroy {
 
   fetchMenu() {
     this.store.dispatch(loadMenus());
-    this.menus$ = this.store.pipe(select(menus));
+    this.menus$ = this.store.pipe(
+      select(menus),
+      filter(isNotEmpty),
+      tap(() => this.computeDistanceFromOriginForEachIonSegmentButton()),
+      tap((fetchedMenus) => this.updateCurrentSegment(fetchedMenus[0]._id)),
+      shareReplay({ refCount: true, bufferSize: 1 })
+    );
     this.cd.detectChanges();
   }
 
@@ -51,9 +57,19 @@ export class MenuPage implements OnInit, OnDestroy {
   handleSlideChange(slides: IonSlides) {
     const activeIndex$ = from(slides.getActiveIndex());
     const selectedMenu$ = activeIndex$.pipe(switchMap((activeIndex) => this.menus$.pipe(map((menuz) => menuz[activeIndex]))));
-    const currentId$ = selectedMenu$.pipe(map((menu) => menu._id));
+    const currentId$ = selectedMenu$.pipe(
+      map((menu) => menu._id),
+      shareReplay({ refCount: true, bufferSize: 1 })
+    );
     this.subs.add(currentId$.pipe(tap((id) => this.updateCurrentSegment(id))).subscribe());
     this.subs.add(currentId$.pipe(tap((id) => this.moveCurrentSegmentIntoView(id))).subscribe());
+  }
+
+  private computeDistanceFromOriginForEachIonSegmentButton() {
+    this.initalItems = Array.from(this.document.querySelectorAll('ion-segment-button')).map((item, idx, original) => ({
+      element: item,
+      distanceFromOrigin: original.reduce((acc, curr, i) => (i >= idx ? acc : acc + curr.getBoundingClientRect().width), 0),
+    }));
   }
 
   private updateCurrentSegment(id: string) {
@@ -61,10 +77,9 @@ export class MenuPage implements OnInit, OnDestroy {
   }
 
   private moveCurrentSegmentIntoView(index: string) {
-    this.document?.querySelector('#segment-'.concat(index))?.scrollIntoView({
+    this.document.querySelector('ion-segment').scrollTo({
+      left: this.initalItems.find((item) => item.element.id.includes(index)).distanceFromOrigin,
       behavior: 'smooth',
-      block: 'center',
-      inline: 'center',
     });
   }
 
