83baeb4aa336d382f5829e6101d00312e18dbee3
smtaha512 <s.m.taha10@gmail.com>
2021-06-25 13:33:13 +0500

    refactor(server): stringify stripe request body and event for better logs

--- server/src/routes/webhooks/stripe/index.js
+++ server/src/routes/webhooks/stripe/index.js
@@ -9,10 +9,10 @@ router.post('/stripe', express.raw({ type: 'application/json' }), (request, resp
   const payload = request.body;
   const stripeSignature = request.headers['stripe-signature'];
 
-  console.log('Got payload: ' + payload);
+  console.log('Got payload: ', JSON.stringify(payload, null, 2));
   try {
     const event = stripe().constructEvent(payload, stripeSignature);
-    console.log(event);
+    console.log(JSON.stringify(event, null, 2));
     repositories.orders.updateOrderStatusByPaymentIntentId(dbModels)(payload.id);
     response.status(200);
   } catch (error) {
