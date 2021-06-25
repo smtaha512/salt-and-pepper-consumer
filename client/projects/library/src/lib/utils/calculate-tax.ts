5c4f68f167962f520d09d7f7a65c377d05664c7e
smtaha512 <s.m.taha10@gmail.com>
2021-06-25 20:27:40 +0500

    fix(library): use round instead of floor in tax calculation

--- client/projects/library/src/lib/utils/calculate-tax.ts
+++ client/projects/library/src/lib/utils/calculate-tax.ts
@@ -1,6 +1,6 @@
 export function calculateTax(subTotal: number, taxPercent: number) {
   // @see https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
-  return Math.floor(((subTotal * taxPercent) / 100) * 100) / 100;
+  return Math.round(((subTotal * taxPercent + Number.EPSILON) / 100) * 100) / 100;
 }
 
 export function calculateTaxCurry(taxPercent: number) {
