5a79a0735fdea6874457053d3f153f06095c00c3
smtaha512 <s.m.taha10@gmail.com>
2021-07-03 01:04:09 +0500

    build(client): update capacitor/android

--- client/projects/consumer/android/app/src/main/java/com/saltandpepper/consumer/app/MainActivity.java
+++ client/projects/consumer/android/app/src/main/java/com/saltandpepper/consumer/app/MainActivity.java
@@ -11,17 +11,4 @@ import ca.zyra.capacitor.stripe.Stripe;
 
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-
-      add(Stripe.class);
-
-    }});
-  }
 }
