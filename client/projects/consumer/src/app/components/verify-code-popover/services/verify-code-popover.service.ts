a1e4a2cc7667b00e8b14f96a5031b58baf7ac3e1
smtaha512 <s.m.taha10@gmail.com>
2021-08-14 00:27:12 +0200

    feat(consumer): use alert controller to show verify input

--- client/projects/consumer/src/app/components/verify-code-popover/services/verify-code-popover.service.ts
+++ client/projects/consumer/src/app/components/verify-code-popover/services/verify-code-popover.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 import { from } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
+import { VerifyCodePopoverDismissRoleEnum } from '../models/verify-code-popover-dismiss-role.enum';
+import { OverlayEventDetail } from '@ionic/core';
+
+interface CodeDictionary {
+  code: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class VerifyCodePopoverService {
-  constructor(private readonly popoverController: PopoverController) {}
+  constructor(private readonly alertController: AlertController) {}
 
   present() {
-    return from(import('../verify-code-popover.component')).pipe(
-      map((c) => c.VerifyCodePopoverComponent),
-      switchMap((component) => from(this.popoverController.create({ component, cssClass: ['popover-center'] }))),
+    return from(
+      this.alertController.create({
+        animated: true,
+        backdropDismiss: false,
+        buttons: [{ text: 'Verify', role: VerifyCodePopoverDismissRoleEnum.VERIFY, handler: ({ code }: CodeDictionary) => ({ code }) }],
+        header: 'Verification',
+        inputs: [{ type: 'number', placeholder: 'Verification code', name: 'code' }],
+        keyboardClose: false,
+        subHeader: 'Enter verification code sent on your phone number',
+      })
+    ).pipe(
       tap((popover) => popover.present()),
-      switchMap((popover) => from(popover.onDidDismiss()))
+      switchMap((popover) => from(popover.onDidDismiss())),
+      map(({ data: { values, ...data }, ...detail }: OverlayEventDetail<CodeDictionary & Record<'values', CodeDictionary>>) => ({
+        ...detail,
+        data,
+      }))
     );
   }
 }
