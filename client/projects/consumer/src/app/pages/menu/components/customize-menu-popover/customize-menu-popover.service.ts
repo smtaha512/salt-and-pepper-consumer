b1fc5cd563f6f0104ba823ccd69737c585ae6cc5
smtaha512 <s.m.taha10@gmail.com>
2021-08-16 00:25:10 +0200

    feat(consumer): change styles for menu customizer

--- client/projects/consumer/src/app/pages/menu/components/customize-menu-popover/customize-menu-popover.service.ts
+++ client/projects/consumer/src/app/pages/menu/components/customize-menu-popover/customize-menu-popover.service.ts
@@ -1,12 +1,45 @@
 import { Injectable } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { select, Store } from '@ngrx/store';
+import { from } from 'rxjs';
+import { first, map, switchMap, tap } from 'rxjs/operators';
+import { togglePakistaniMenu } from '../../../../+state/user/user.actions';
+import { showPakistaniMenu } from '../../../../+state/user/user.selectors';
 
 @Injectable({ providedIn: 'root' })
 export class CustomizeMenuPopoverService {
-  constructor(private readonly popoverController: PopoverController) {}
+  constructor(private readonly alertController: AlertController, private readonly store: Store<any>) {}
 
   async present() {
-    const component = await import('./customize-menu-popover.component').then((c) => c.CustomizeMenuPopoverComponent);
-    this.popoverController.create({ component }).then((popover) => popover.present());
+    const alert$ = this.store
+      .pipe(
+        select(showPakistaniMenu),
+        first(),
+        switchMap((checked) => from(this.constructAlert(checked))),
+        switchMap((alert) => from(alert.present()).pipe(map(() => alert))),
+        tap((alert) =>
+          alert.onDidDismiss().then(() => {
+            alert$.unsubscribe();
+          })
+        )
+      )
+      .subscribe();
+  }
+
+  constructAlert(checked: boolean) {
+    return this.alertController.create({
+      header: 'Customize Menu',
+      subHeader: 'Show / hide from menu',
+      inputs: [
+        {
+          type: 'checkbox',
+          label: 'Pakistani Menu',
+          name: 'pakistani-menu',
+          checked,
+          handler: (alertInput) => this.store.dispatch(togglePakistaniMenu({ value: alertInput.checked })),
+        },
+      ],
+      buttons: [{ text: 'OK', role: 'cancel' }],
+    });
   }
 }
