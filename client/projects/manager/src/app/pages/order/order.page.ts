167eea4240ee210a00ccbab1269d0a263810c3d2
smtaha512 <s.m.taha10@gmail.com>
2021-08-15 23:05:31 +0200

    feat(manager): disable status buttons based on current status

--- client/projects/manager/src/app/pages/order/order.page.ts
+++ client/projects/manager/src/app/pages/order/order.page.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ActionSheetController, ToastController } from '@ionic/angular';
+import { ActionSheetButton } from '@ionic/core';
 import { OrderInterface, OrderStatausEnum } from 'dist/library';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { first, map, repeatWhen, shareReplay, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { BehaviorSubject, EMPTY, from, Observable } from 'rxjs';
+import { first, map, repeatWhen, shareReplay, switchMap, tap } from 'rxjs/operators';
 import { Printer } from '../../services/printer/printer';
 import { OrdersHistoryService } from '../orders-history/services/orders-history.service';
 
@@ -55,16 +56,28 @@ export class OrderPage implements OnInit, OnDestroy {
   }
 
   async presentActionSheet(currentStatus: OrderStatausEnum) {
+    /** In this record, key represents the button text and value represents all the possible status for which the button will be disabled */
+    const disableStatusMap: Record<OrderStatausEnum, Array<OrderStatausEnum>> = {
+      [OrderStatausEnum.CANCELLED]: [OrderStatausEnum.PICKED],
+      [OrderStatausEnum.PICKED]: [OrderStatausEnum.CANCELLED],
+      [OrderStatausEnum.PREPARED]: [OrderStatausEnum.CANCELLED, OrderStatausEnum.PICKED],
+      [OrderStatausEnum.PREPARING]: [OrderStatausEnum.CANCELLED, OrderStatausEnum.PICKED, OrderStatausEnum.PREPARED],
+    };
+    const statusButtons: ActionSheetButton[] = [
+      { text: OrderStatausEnum.CANCELLED, icon: 'trash-outline', role: 'destructive', cssClass: 'ion-button-text-color-danger' },
+      { text: OrderStatausEnum.PREPARING, icon: 'flame-sharp', cssClass: ['ion-button-text-color-light'] },
+      { text: OrderStatausEnum.PREPARED, icon: 'checkmark-outline' },
+      { text: OrderStatausEnum.PICKED, icon: 'checkmark-done-outline' },
+    ]
+      .map((button) => ({ ...button, cssClass: Array.isArray(button.cssClass) ? [...button.cssClass] : [button.cssClass] }))
+      .map((button) => ({
+        ...button,
+        cssClass: [...button.cssClass, ...(disableStatusMap[button.text].includes(currentStatus) ? ['disable-action-sheet-btns'] : [])],
+      }));
     const actionSheet = await this.actionSheetController.create({
       cssClass: 'ion-color-light',
       header: 'Change order status',
-      buttons: [
-        { text: OrderStatausEnum.CANCELLED, role: 'destructive', cssClass: 'ion-button-text-color-danger', icon: 'trash-outline' },
-        { text: OrderStatausEnum.PREPARING, icon: 'flame-sharp', cssClass: ['ion-button-text-color-light'] },
-        { text: OrderStatausEnum.PREPARED, icon: 'checkmark-outline' },
-        { text: OrderStatausEnum.PICKED, icon: 'checkmark-done-outline' },
-        { text: 'Close', icon: 'close', role: 'cancel' },
-      ]
+      buttons: [...statusButtons, { text: 'Close', icon: 'close', role: 'cancel' }]
         .map((item) =>
           item.text.toLowerCase() !== currentStatus.toLowerCase()
             ? item
@@ -79,8 +92,11 @@ export class OrderPage implements OnInit, OnDestroy {
                   this.currentOrder$
                     .pipe(
                       first(),
+                      switchMap((order) => from(this.cancelOrderConfirmation().then((role) => (role === 'cancel-order' ? order : null)))),
                       switchMap((order) =>
-                        this.ordersHistoryService.update(order._id, { status: item.text as OrderStatausEnum }).pipe(first())
+                        !order
+                          ? EMPTY
+                          : this.ordersHistoryService.update(order._id, { status: item.text as OrderStatausEnum }).pipe(first())
                       ),
                       tap(() => this.refetch$.next(true))
                     )
@@ -91,6 +107,19 @@ export class OrderPage implements OnInit, OnDestroy {
     });
 
     await actionSheet.present();
+  }
+
+  cancelOrderConfirmation() {
+    return this.actionSheetController
+      .create({
+        header: 'Cancel order',
+        subHeader: 'Are you sure you want to cancel this order?',
+        buttons: [
+          { text: 'No', role: 'cancel', cssClass: 'ion-button-text-color-danger' },
+          { text: 'Yes, I am sure', role: 'cancel-order' },
+        ],
+      })
+      .then((alert) => alert.present().then(() => alert.onDidDismiss().then((eventDetail) => eventDetail.role)));
   }
 
   changeOrderStatus(currentStatus: OrderStatausEnum) {
